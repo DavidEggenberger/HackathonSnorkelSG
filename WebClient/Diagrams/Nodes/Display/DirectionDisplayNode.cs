b2761c9b5010c1f9116b88867b7b08082170c4f4
DavidEggenberger <david99.eggenberger@gmail.com>
2021-11-07 02:40:33 +0100

    DirectionDisplayNode

--- WebClient/Diagrams/Nodes/Display/DirectionDisplayNode.cs
+++ WebClient/Diagrams/Nodes/Display/DirectionDisplayNode.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Blazor.Diagrams.Core.Models;
+using Domain.SnorkelAggregate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebClient.Diagrams.Nodes.Display
 {
-    public class DirectionDisplayNode
+    public class DirectionDisplayNode : NodeModel
     {
+        public Snorkel Snorkel { get; set; }
+        public DirectionDisplayNode(Blazor.Diagrams.Core.Geometry.Point position = null) : base(position)
+        {
+
+        }
     }
 }
