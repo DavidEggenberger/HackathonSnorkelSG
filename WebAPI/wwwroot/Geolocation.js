186451644ca95d5f5190de0bfdbacc2e763568cd
DavidEggenberger <david99.eggenberger@gmail.com>
2021-11-07 04:52:36 +0100

    StreetNavigation IFrames

---
+++ WebAPI/wwwroot/Geolocation.js
@@ -0,0 +1,8 @@
+﻿function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(returnPosition);
+    }
+}
+function returnPosition(position) {
+    DotNet.invokeMethod('WebClient', 'SetUserLocation', position.coords.latitude, position.coords.longitude);
+}
